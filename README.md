90f3e4d6924773fe2c1f1034eb9508a16ca3ec55
josepabellana <josep.abe.pu@gmail.com>
2024-05-20 11:58:00 +0200

    Calculator code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # installerDemo
+
+```
+Go to Main.java and run the app
+```
+
---
+++ calculator/src/main/java/josep/calculator/Main.java
@@ -0,0 +1,10 @@
+package josep.calculator;
+
+import josep.calculator.ui.CalculatorUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
